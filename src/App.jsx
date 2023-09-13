6fbd8dc30501887ddf202928c53dea724ff3deb3
jorge <jorge.gomes6793@gmail.com>
2023-09-13 19:02:57 -0300

    fix: style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
-import './App.css'
-import fotoPerfil from "./imagens/perfil.png"
+import './App.css';
+import fotoPerfil from "./imagens/perfil.png";
+import ProjetoPixelArte from "./imagens/pixelArt.png";
+import ProjetoRecipes from "./imagens/Recipes.png";
+import ProjetoTrunfo from "./imagens/trunfo.png";
+import emBreve from "./imagens/emBreve.jpeg"
 
 function App() {
   const [theme, setTheme] = useState('light');
@@ -31,48 +35,74 @@ function App() {
       </section>
       <h1>Projetos</h1>
       <section className='project'>
+        <div className='project-container'>
+        <img width="350"  src={ProjetoPixelArte} alt="imagem do projeto pixels Arte" />
         <div>
           Pixel Arte
         </div>
+        </div>
+        <div className='project-container'>
+        <img width="350"  src={ProjetoRecipes} alt="imagem do projeto Receitas" />
         <div>
           Site Receitas
         </div>
+        </div>
+        <div className='project-container'>
+        <img width="350"  src={ProjetoTrunfo} alt="imagem do projeto trunfo" />
         <div>
           Trunfo
         </div>
+        </div>
+        <div className='project-container'>
+        <img width="350"  src={emBreve} alt="imagem do projeto pixels Arte" />
         <div>
           React Native(Em breve)
+        </div>
         </div>
       </section>
       <h1>Habilidades</h1>
       <section className='skills'>
         <div className='imagem-container'>
           <img width="48" height="48" src="https://img.icons8.com/color/48/html-5--v1.png" alt="html-5--v1"/>
-          HTML5
+          <div>
+            HTML5
+          </div>
         </div>
         <div className='imagem-container'>
           <img className='imagem-container' width="48" height="48" src="https://img.icons8.com/color/48/css3.png" alt="css3" />
-          CSS3
+          <div>
+            CSS3
+          </div>
         </div>
         <div className='imagem-container'>
           <img className='imagem-container' width="48" height="48" src="https://img.icons8.com/color/48/javascript--v1.png" alt="javascript--v1" />
-          JavaScript
+          <div>
+            JavaScript
+          </div>
         </div>
         <div className='imagem-container'>
           <img className='imagem-container' width="48" height="48" src="https://img.icons8.com/color/48/react-native.png" alt="react-native" />
-          React
+          <div>
+            React
+          </div>
         </div>
         <div className='imagem-container'>
           <img className='imagem-container' width="48" height="48" src="https://img.icons8.com/color/48/redux.png" alt="redux" />
-          Redux
+          <div>
+            Redux
+          </div>
         </div>
         <div className='imagem-container'>
           <img className='imagem-container' width="48" height="48" src="https://img.icons8.com/fluency/48/node-js.png" alt="node-js" />
-          Node.js
+          <div>
+            Node.js
+          </div>
         </div>
         <div className='imagem-container'>
           <img className='imagem-container' width="48" height="48" src="https://img.icons8.com/color/48/git.png" alt="git" />
-          Git
+          <div>
+            Git
+          </div>
         </div>
       </section>
     </body >
